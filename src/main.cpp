8186274596bc09db9a68d8ed886c7fd02d7aa4bc
Christian Piper <xmanpiper@gmail.com>
2019-05-06 16:32:33 -0400

    Removed PPM and iBUS, samples PWM now

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+
 #include <Arduino.h>
 #include <PPM.hpp>
 #include <iBUStelemetry.h>
@@ -6,16 +7,62 @@
 int iBUS_pin = 4;
 int throttle_pin = 3;
 
+bool Serial_output = true;
+
+
 /*
 Mappings are as follows:
-7 - ON/OFF switch - Potentiometer power adjustment | 25% power | Full power
-8 - 2nd potentiometer - For power adjustment
+5 - ON/OFF switch - Potentiometer power adjustment | 25% power | Full power
+6 - 2nd potentiometer - For hydrofoil adjustment
 
 */
 
-PPM reciever;
-iBUStelemetry telemetry(iBUS_pin);
+//PPM reciever;
+//iBUStelemetry telemetry(iBUS_pin);
 
+void applyThrottle() {
+  float ch1 = map(pulseIn(2, HIGH), 0, 2000, 0, 255);
+  float lastCh1 = 0;
+  int resetCounts;
+  float ch5 = map(pulseIn(3, HIGH), 0, 2000, 0, 255);
+
+  ch1 = (ch1 + lastCh1) / 2;
+
+  if (ch5 == 255) {
+
+    analogWrite( throttle_pin , (ch1 * .25) ); //25% power
+
+  }
+
+  else if (ch5 == 0) {
+
+    analogWrite( throttle_pin , (ch1 * .50) ); //50% power
+    
+  }
+
+  else {
+
+    analogWrite( throttle_pin , ch1 ); //100% power
+
+  }
+
+  if (Serial_output) {
+    Serial.print(ch1);
+    Serial.print(", ");
+    Serial.print(ch5);
+    Serial.println();
+  }
+
+}
+
+
+
+
+
+
+
+
+/*
 void applyThrottle() {
 
   if (reciever.getValue(7) == 0 ) {
@@ -38,23 +85,29 @@ void applyThrottle() {
   }
 
 }
+*/
 
+
+/*
 void writeSensorData() {
   //Do some stuff to figure out telemetry
   telemetry.setSensorMeasurement(1, 1.2);
 
 }
+*/
 
 void setup() {
   // put your setup code here, to run once:
-  reciever.begin(2,1);
-  telemetry.begin(115200);
-  telemetry.addSensor(0);
-
+  //reciever.begin(2,1);
+  //telemetry.begin(115200);
+  //telemetry.addSensor(0);
+  if (Serial_output) {
+    Serial.begin(115200);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   applyThrottle();
-  writeSensorData();
 }
+
