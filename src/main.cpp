c36774b386d750ec5b3c168bc4394cfa7700c065
unknown <xmanpiper@gmail.com>
2019-08-08 15:14:34 -0400

    Adjusted threshold for steering

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int ch5_pin = 6;
 int ch6_pin = 7;
 //Initialize variables for channel values
 float ch1 = 0; //Steering
-float ch1_raw = 0;
+
 float ch3 = 0; //Throttle
 float ch5 = 0; //3-way switch
 float ch6 = 0; //VrB
@@ -28,15 +28,13 @@ rampFloat R_motor_ramp;
 
 
 void calculateThrottle() {
-
-  ch1_raw = pulseIn( ch1_pin, HIGH);
-  ch1 = map ( ch1_raw, 986, 1972, -100, 100 );// + 2; 
+  ch1 = map ( pulseIn( ch1_pin, HIGH), 986, 1972, -100, 100 );// + 2; 
   ch3 = map ( pulseIn( ch3_pin, HIGH), 993, 1990, 0, 255 );
   ch5 = int ( map ( pulseIn( ch5_pin, HIGH), 500, 1500, 0, 2 )); 
   ch6 = map ( pulseIn( ch6_pin, HIGH), 994, 1988, 0, 1000 );
 
 
-  if (ch1 < 2 || ch1 > -2) { //Steering is moved
+  if (ch1 < 3 || ch1 > -3) { //Steering is moved
     //Steering Calculations
     if ( ch1 > 0 ) { //Stick moved to the right, therefore boat should turn right. This is done by slowing down the right motor.
       L_motor_value = ch3;
@@ -97,20 +95,18 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   calculateThrottle();
-  //Set pins to the values calculated
-  analogWrite( L_motor_pin, L_motor_value );
-  analogWrite( R_motor_pin, R_motor_value );
+  //Set pins to the values calculated if they are above the threshold
+  if ( L_motor_value > 75 ) {
+    analogWrite( L_motor_pin, L_motor_value );
+  }
+  if ( R_motor_value > 75 ) {
+    analogWrite( R_motor_pin, R_motor_value );
+  }
   //Write out Serial data if enabled
   if (Serial_output) {
     Serial.print( L_motor_value );
     Serial.print(", ");
     Serial.print( R_motor_value );
-    Serial.print(", ");
-    Serial.print( 200 - abs(ch1) );
-    Serial.print(", ");
-    Serial.print( 1.5 - ( 100 / ( 200 - abs(ch1) ) ) );
-    Serial.print(", ");
-    Serial.print( ch1_raw );
     Serial.println();
   }
 }
